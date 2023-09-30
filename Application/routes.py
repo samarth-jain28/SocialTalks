2890c1763151089d6f89cb3da71014fa5a39c78c
Samarth Jain <samarthjain28.2002@gmail.com>
2023-09-30 11:48:52 +0530

    co

--- Application/routes.py
+++ Application/routes.py
@@ -1,15 +1,8 @@
+from flask import render_template
 from Application import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'sam'}
-    return '''
-    <html>
-        <head>
-            <title>HomePage-SocialTalks</title>
-        </head>
-        <body>
-            <h1>Hello, ''' +user['username']+ '''!</h1>
-        </body>
-    </html> '''
+    return render_template('index.html',title='HomePage', user=user)
