8249547b75bf3b90be47a943687cdcba3ddcf6b5
Samarth Jain <samarthjain28.2002@gmail.com>
2023-10-30 18:13:32 +0530

    reciving data

--- Application/routes.py
+++ Application/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template , flash , redirect ,url_for
 from Application import app
-from Application.form import LoginForm
+from Application.form import LoginForm 
 
 @app.route('/')
 @app.route('/index')
@@ -21,4 +21,7 @@ def index():
 @app.route('/login' , methods=['GET' , 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {} , remember me={}'.format(form.username.data,form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html' ,title='SignIn' ,form = form)
